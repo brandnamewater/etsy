842de273a864f66cb086dfb05e5bfe2b40a2c940
brandnamewater <brandnamewater@gmail.com>
2018-09-12 21:16:44 -0400

    tied listings to users

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,10 @@
 class Listing < ApplicationRecord
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "jimmycaryy.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  validates :name, :description, :price, :response_time, presence: true
+  validates :price, numericality: { greater_than: 1 }
+  validates_attachment_presence :image
+
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_12_082109) do
+ActiveRecord::Schema.define(version: 2018_09_13_005550) do
 
   create_table "listings", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2018_09_12_082109) do
     t.string "image_content_type"
     t.integer "image_file_size"
     t.datetime "image_updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,6 +35,7 @@ ActiveRecord::Schema.define(version: 2018_09_12_082109) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
